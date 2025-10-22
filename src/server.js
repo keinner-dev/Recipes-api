e439f8ef68cf6ff6856f48a0f85430d0282933ee
Keinner <foreverkeinner@gmail.com>
2025-10-22 18:29:30 -0400

    first commit

--- src/server.js
+++ src/server.js
@@ -4,10 +4,13 @@ import { ENV } from "./config/env.js";
 import { db } from "./config/db.js"
 import { favoritesTables } from "./db/schema.js";
 import { and, eq } from "drizzle-orm";
+import job from "./config/cron.js";
 
 
 const app = express()
 const PORT = ENV.PORT || 5001
+
+if(ENV.NODE_ENV === "production") job.start()
 
 app.use(express.json())
 
